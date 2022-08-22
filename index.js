e68e7de1761cf599744fe4c4a0c8c095fcc6e101
Your Name <tpunchihewage@gmail.com>
2022-08-22 15:17:59 +0200

    added JS files

---
+++ index.js
@@ -0,0 +1,28 @@
+// Exercise 01
+console.log("  -----------------------------------");
+console.log("   ++++++++++ 01 Exercise ++++++++++");
+
+console.log("Undefined & 2");
+console.log("Because a is not declare and its have nothing to show");
+
+// Exercise 02
+console.log("  -----------------------------------");
+console.log("   ++++++++++ 02 Exercise ++++++++++");
+
+console.log(
+  'result will be 5, because its returns the value second declaration value " a"'
+);
+
+// Exercise 03
+console.log("  -----------------------------------");
+console.log("   ++++++++++ 03 Exercise ++++++++++");
+
+console.log(
+  'results will be "Aurello De Rosa" & "udefined" because its function Object and first console.log had been call it correctly  the second have not. thats why first console log get value and "test()function get undefined"'
+);
+
+// Exercise 04
+console.log("  -----------------------------------");
+console.log("   ++++++++++ 04 Exercise ++++++++++");
+
+console.log('Value of "a"');
